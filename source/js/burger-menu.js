31c99b60d327f43f3c32a22b0f2351cfb31e09f5
RenaDesu <rena-desu@mail.ru>
2023-03-29 23:23:06 +0300

    Добавляет стили на главную 1, мобильная версия

---
+++ source/js/burger-menu.js
@@ -0,0 +1,19 @@
+const navMain = document.querySelector('.main-nav');
+const headerToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+headerToggle.classList.remove('main-header__toggle--nojs');
+
+headerToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    headerToggle.classList.remove('main-header__toggle--nav-closed');
+    navMain.classList.add('main-nav--opened');
+    headerToggle.classList.add('main-header__toggle--nav-opened')
+  } else {
+    navMain.classList.add('main-nav--closed');
+    headerToggle.classList.add('main-header__toggle--nav-closed')
+    navMain.classList.remove('main-nav--opened');
+    headerToggle.classList.remove('main-header__toggle--nav-opened')
+  }
+});
