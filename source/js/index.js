c7782d5343d55868c61a0e2cc9e38ca4aebd8fb4
RenaDesu <rena-desu@mail.ru>
2023-04-27 18:17:53 +0300

    Добавляет спрайт + наметки слайдера

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,12 @@
 const navMain = document.querySelector('.main-nav');
 const headerToggle = document.querySelector('.main-header__toggle');
+const buttonMinus = document.querySelector('#button-minus');
+const buttonPlus = document.querySelector('#button-plus');
+const imgBefore = document.querySelector('#img-before');
+const imgAfter = document.querySelector('#img--after');
+const sliderValue = document.querySelector('.slider__value');
 
+// Burger menu
 navMain.classList.remove('main-nav--nojs');
 headerToggle.classList.remove('main-header__toggle--nojs');
 
@@ -17,3 +23,5 @@ headerToggle.addEventListener('click', function () {
     headerToggle.classList.remove('main-header__toggle--nav-opened')
   }
 });
+
+
