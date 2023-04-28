eab01311e485e6a39477b50e1736f27c0dcd092b
RenaDesu <rena-desu@mail.ru>
2023-04-28 14:02:53 +0300

    Добавляет слайдер на главную

---
+++ source/js/cat-slider.js
@@ -0,0 +1,78 @@
+// Cat slider
+
+const slider = document.querySelector('.slider');
+const before = slider.querySelector('.slider__before');
+const beforeImage = before.querySelector('img');
+const changeBar = slider.querySelector('.slider__change-bar');
+const body = document.body;
+
+let isActive = false;
+
+document.addEventListener('DOMContentLoaded', () => {
+  let width = slider.offsetWidth;
+  beforeImage.style.width = `${width}px`;
+});
+
+const beforeAfterSlider = (x) => {
+  let shift = Math.max(0, Math.min(x, slider.offsetWidth));
+  before.style.width = `${shift}px`;
+  changeBar.style.left = `${shift}px`;
+};
+
+const pauseEvents = (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  return false;
+};
+
+body.addEventListener('mousedown', () => {
+  isActive = true;
+});
+
+body.addEventListener('mouseup', () => {
+  isActive = false;
+});
+
+body.addEventListener('mouseleave', () => {
+  isActive = false;
+});
+
+body.addEventListener('mousemove', (e) => {
+  if (!isActive) {
+    return;
+  }
+
+  let x = e.pageX;
+  x -= slider.getBoundingClientRect().left;
+  beforeAfterSlider(x);
+  pauseEvents(e);
+});
+
+body.addEventListener('touchstart', () => {
+  isActive = true;
+});
+
+body.addEventListener('touchend', () => {
+  isActive = false;
+});
+
+body.addEventListener('touchcancel', () => {
+  isActive = false;
+});
+
+body.addEventListener('touchmove', (e) => {
+  if (!isActive) {
+    return;
+  }
+
+  let x;
+  let i;
+
+  for (i = 0; i < e.changedTouches.length; i++) {
+    x = e.changedTouches[i].pageX;
+  }
+
+  x -= slider.getBoundingClientRect().left;
+  beforeAfterSlider(x);
+  pauseEvents(e);
+});
--- source/js/index.js
+++ source/js/index.js
@@ -1,10 +1,5 @@
 const navMain = document.querySelector('.main-nav');
 const headerToggle = document.querySelector('.main-header__toggle');
-const buttonMinus = document.querySelector('#button-minus');
-const buttonPlus = document.querySelector('#button-plus');
-const imgBefore = document.querySelector('#img-before');
-const imgAfter = document.querySelector('#img--after');
-const sliderValue = document.querySelector('.slider__value');
 
 // Burger menu
 navMain.classList.remove('main-nav--nojs');
