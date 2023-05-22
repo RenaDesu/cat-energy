f7213dae0135af823871e923a3a8db46bf9845ce
RenaDesu <rena-desu@mail.ru>
2023-05-22 19:05:00 +0300

    Добавляет управление каталогом 2

--- source/js/catalog.js
+++ source/js/catalog.js
@@ -1,18 +1,23 @@
-const productListData = [
+const PRODUCT_LIST_DATA = [{
+    imgUrl: 'img/rice-mobile-min.png',
+    imgUrl2x: 'img/rice-mobile-min@2x.png',
+    imgUrlTab: 'img/rice-tablet-min.png',
+    imgUrlTab2x: 'img/rice-tablet-min@2x.png',
+    title: 'Cat Energy slim 500 г',
+    weight: '500 г',
+    flavor: 'Рис+',
+    price: '500 Р.'
+  },
   {
-  imgUrl: 'img/fish-mobile-max.png',
-  title: 'Cat Energy slim 1000 г',
-  weight: '1000 г',
-  flavor: 'Рис',
-  price: '1000 Р.'
-},
-{
-  imgUrl: 'img/chicken-mobile-min.png',
-  title: 'Cat Energy slim 500 г',
-  weight: '500 г',
-  flavor: 'Индейка',
-  price: '500 Р.'
-},
+    imgUrl: 'img/chicken-mobile-min.png',
+    imgUrl2x: 'img/chicken-mobile-min@2x.png',
+    imgUrlTab: 'img/chicken-tablet-min.png',
+    imgUrlTab2x: 'img/chicken-tablet-min@2x.png',
+    title: 'Cat Energy slim 500 г',
+    weight: '500 г',
+    flavor: 'Индейка',
+    price: '500 Р.'
+  },
 ]
 
 const productList = document.querySelector('.catalog__list');
@@ -21,11 +26,23 @@ const productListFragment = document.createDocumentFragment();
 const showAllButton = productList.querySelector('.product-list__button-all');
 const itemMore = productList.querySelector('#item-more');
 
-
 const createProductListItem = () => {
-  productListData.forEach((product) => {
+  PRODUCT_LIST_DATA.forEach((product) => {
     const productListElement = productListItemTemplate.cloneNode(true);
-    productListElement.querySelector('.product-list__product-image').src = product.imgUrl;
+    if (window.matchMedia('(min-width: 768px)').matches) {
+      if (window.matchMedia('(min-resolution: 2dppx)').matches) {
+        productListElement.querySelector('.product-list__product-image').src = product.imgUrlTab2x;
+    } else {
+      productListElement.querySelector('.product-list__product-image').src = product.imgUrlTab;
+    }
+
+    } else {
+      if (window.matchMedia('(min-resolution: 2dppx)').matches) {
+        productListElement.querySelector('.product-list__product-image').src = product.imgUrl2x;
+      } else {
+        productListElement.querySelector('.product-list__product-image').src = product.imgUrl;
+      }
+    }
     productListElement.querySelector('.product-list__title').textContent = product.title;
     productListElement.querySelector('#weight-value').textContent = product.weight;
     productListElement.querySelector('#flavor-value').textContent = product.flavor;
